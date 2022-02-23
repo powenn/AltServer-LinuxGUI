42a59c456d409e933cf07a7b629f78a27e7f92a9
powenn <powen0966091716@gmail.com>
2022-02-23 08:46:55 -0500

    testing

--- update.py
+++ update.py
@@ -3,9 +3,13 @@
 # Author of the script : powen
 
 # import 
+from platform import release
 import subprocess
+import os
 
+cwd = os.getcwd()
+release_path = cwd+"/AltServerGUI"
 LatestVersion=subprocess.check_output("curl -Lsk https://github.com/powenn/AltServer-LinuxGUI/raw/main/version",shell=True).decode('utf-8')
 
-GetReleaseCMD='curl https://github.com/powenn/AltServer-LinuxGUI/releases/download/%s/AltServerGUI.zip' %LatestVersion
+GetReleaseCMD='curl -L https://github.com/powenn/AltServer-LinuxGUI/releases/download/%s/AltServerGUI > %s' %(LatestVersion,release_path)
 subprocess.run(GetReleaseCMD,shell=True)
