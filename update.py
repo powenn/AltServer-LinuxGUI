0f496c4de7e3b3bf3a10cd24b48abee612c66aa3
powenn <powen0966091716@gmail.com>
2022-02-23 21:00:15 -0500

    testing

--- update.py
+++ update.py
@@ -7,10 +7,10 @@ import subprocess
 import os
 
 cwd = os.getcwd()
-release_path = cwd+"/AltServerGUI"
 LatestVersion=subprocess.check_output("curl -Lsk https://github.com/powenn/AltServer-LinuxGUI/raw/main/version",shell=True).decode('utf-8')
 ReleaseName = cwd+"/AltServerGUI-new"
 
 GetReleaseCMD='curl -L https://github.com/powenn/AltServer-LinuxGUI/releases/download/%s/AltServerGUI > %s' %(LatestVersion,ReleaseName)
+
 subprocess.run(GetReleaseCMD,shell=True)
 subprocess.run("chmod +x %s" %ReleaseName,shell=True)
