7476972d541e10bdb951d141d0720d3f47c99699
powenn <powen0966091716@gmail.com>
2022-03-16 09:10:39 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")" || exit
 
 pyinstaller -w -n altserver UI.py
 cp -R ./resources ./dist/altserver
-cp -R ./dist/altserver ./AltServer/usr/lib/altserver
+cp -R ./dist/altserver ./AltServer/usr/lib
 dpkg -b AltServer AltServer.deb
