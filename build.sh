f51dbccff4a280e78eb45a9c4aab9f02d5c472bc
powenn <powen0966091716@gmail.com>
2022-03-16 11:12:26 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ cd "$(dirname "$0")" || exit
 
 pyinstaller -w -n altserver UI.py
 cp -R ./resources ./dist/altserver
+if [ ! -d "./AltServer/usr/lib" ]; then
+    mkdir "./AltServer/usr/lib"
+fi
+
 cp -R ./dist/altserver ./AltServer/usr/lib
 dpkg -b AltServer AltServer.deb
