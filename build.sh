9995360a062079b361f64c74929435307e8aacee
powenn <powen0966091716@gmail.com>
2022-03-21 07:21:56 +0800

    test build.yml

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 
 cd "$(dirname "$0")" || exit
 
+if [ -d "./AltServer/usr/lib" ]; then
+    rm -rf "./AltServer/usr/lib"
+fi
+if [ -d "./dist" ]; then
+    rm -rf "./dist"
+fi
+
 pyinstaller -w -n altserver UI.py
 cp -R ./resources ./dist/altserver
 if [ ! -d "./AltServer/usr/lib" ]; then
