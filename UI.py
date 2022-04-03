7006a2c3d9b1a29af33b9a24406bf618178dda65
nhubao <baoprokute1234@gmail.com>
2022-04-03 22:58:03 +0700

    Initial PKGBUILD

--- UI.py
+++ UI.py
@@ -1,15 +1,20 @@
 # AltServer-Linux GUI wrote in PyQT5
-# This is the UI part 
+# This is the UI part
 # Author of the GUI : powen
 
 # import
 from Main import *
 
+HOME = os.path.expanduser("~")
+AUTOSTART_PATH = os.path.join(HOME, ".config/autostart/AltServer.desktop")
+IS_AUTOSTART = os.path.exists(AUTOSTART_PATH)
+
 # check permission
-if not os.access(AltServer, os.X_OK):
-    subprocess.run(f"chmod +x {AltServer}", shell=True)
-if not os.access(AutoStart, os.X_OK):
-    subprocess.run(f"chmod +x {AutoStart}", shell=True)
+if using_resource_altserver:
+    if not os.access(AltServer, os.X_OK):
+        subprocess.run(f"chmod +x {AltServer}", shell=True)
+    if not os.access(AutoStart, os.X_OK):
+        subprocess.run(f"chmod +x {AutoStart}", shell=True)
 
 # UI part
 app = QApplication(sys.argv)
@@ -33,34 +38,37 @@ menu.addAction(About)
 menu.addSeparator()
 
 # Add Inatall AltStore option to the menu.
-AltInstall = QAction('Install AltStore')
+AltInstall = QAction("Install AltStore")
 AltInstall.triggered.connect(Installation)
 menu.addAction(AltInstall)
 menu.addSeparator()
 
 # Add Launch At Login in  option to the menu.
-LaunchAtLogin = QAction("Launch at Login",checkable=True)
-launch_enable=False
-CheckTime=0
-if glob.glob("/home/*/.config/autostart/AltServer.desktop") and CheckTime==0:
+LaunchAtLogin = QAction("Launch at Login", checkable=True)
+launch_enable = False
+CheckTime = 0
+if IS_AUTOSTART and CheckTime == 0:
     LaunchAtLogin.setChecked(True)
-    launch_enable=True
-    CheckTime=1
-if not glob.glob("/home/*/.config/autostart/AltServer.desktop") and CheckTime==0:
+    launch_enable = True
+    CheckTime = 1
+if not IS_AUTOSTART and CheckTime == 0:
     LaunchAtLogin.setChecked(False)
-    launch_enable=False
-    CheckTime=1
-def launch_config() :
+    launch_enable = False
+    CheckTime = 1
+
+
+def launch_config():
     if LaunchAtLogin.isChecked():
-        launch_enable=True
+        launch_enable = True
     if not LaunchAtLogin.isChecked():
-         launch_enable=False
-    if launch_enable :
+        launch_enable = False
+    if launch_enable:
         LaunchAtLogin.setChecked(True)
-        subprocess.run(resource_path("AutoStart.sh"),shell=True)
-    if not launch_enable :
+        subprocess.run(resource_path("AutoStart.sh"), shell=True)
+    if not launch_enable:
         LaunchAtLogin.setChecked(False)
-        subprocess.run("rm -rf /home/*/.config/autostart/AltServer.desktop",shell=True)
+        os.remove(AUTOSTART_PATH)
+
 
 LaunchAtLogin.toggled.connect(launch_config)
 menu.addAction(LaunchAtLogin)
@@ -84,16 +92,18 @@ menu.addSeparator()
 
 # Add a Quit option to the menu.
 def app_quit():
-    subprocess.run(f'killall {AltServer}',shell=True)
-    app.quit()    
+    subprocess.run(f"killall {AltServer}", shell=True)
+    app.quit()
+
+
 quit = QAction("Quit AltServer")
 quit.triggered.connect(app_quit)
 quit.setCheckable(False)
-quit.setShortcut('Ctrl+Q')
+quit.setShortcut("Ctrl+Q")
 menu.addAction(quit)
 
 # Add the menu to the tray
 tray.setContextMenu(menu)
-subprocess.run(f'{AltServer} &> /dev/null &',shell=True)
+subprocess.run(f"{AltServer} &> /dev/null &", shell=True)
 UpdateNotification()
 app.exec_()
--- update.py
+++ update.py
@@ -1,11 +1,14 @@
 # AltServer-Linux GUI wrote in PyQT
-# This is the update part 
+# This is the update part
 # Author of the script : powen
 
-# import 
+# import
 import subprocess
+import requests
 
-LatestVersion=subprocess.check_output("curl -Lsk https://github.com/powenn/AltServer-LinuxGUI/raw/main/version",shell=True).decode('utf-8')
-GetReleaseCMD='curl -L https://github.com/powenn/AltServer-LinuxGUI/releases/download/%s/AltServer.deb > AltServer.deb' %LatestVersion
+LatestVersion = requests.get(
+    "https://github.com/powenn/AltServer-LinuxGUI/raw/main/version"
+).text
+GetReleaseCMD = f"curl -L https://github.com/powenn/AltServer-LinuxGUI/releases/download/{LatestVersion}/AltServer.deb > /tmp/AltServer.deb"
 
-subprocess.run(GetReleaseCMD,shell=True)
+subprocess.run(GetReleaseCMD, shell=True)
