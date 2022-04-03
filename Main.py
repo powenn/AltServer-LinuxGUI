723065ef1b01e0db30265445fa6a4b87e64a27e5
nhubao <baoprokute1234@gmail.com>
2022-04-03 23:23:52 +0700

    Fix AltStore binary location

--- Main.py
+++ Main.py
@@ -110,7 +110,7 @@ def Installation():
             AccountArea.close()
             AppleID = IDInputArea.text()
             Password = PasswordInputArea.text()
-            InsAltStoreCMD = f'{resource_path("AltServer")} -u {UDID} -a {AppleID} -p {Password} {PATH} > {TMP_FILE}'
+            InsAltStoreCMD = f'{AltServer} -u {UDID} -a {AppleID} -p {Password} {PATH} > {TMP_FILE}'
             Installing = True
             WarnTime = 0
             InsAltStore = subprocess.Popen(
